22fa8ed731db2ddc52abbb39460b59dec93ba246
OtiliaNicola <otilianicola94@gmail.com>
2025-01-09 13:32:26 +0100

    fix: add window event listener

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-const screen = document.querySelector(".screen");
-const buttons = document.querySelectorAll(".btn");
 
-buttons.forEach( (btn) => {
+window.addEventListener("load", ()=>{
+    const screen = document.getElementById("screen");
+    const buttons = document.querySelectorAll(".btn");
     
-    btn.addEventListener("click", () => {
+    buttons.forEach( (btn) => {
         
-        const btnPushed = btn.textContent;
-
-        if(btn.id === "c") {
-            screen.textContent = "0";
-            return;
-        }
-
-        if(btn.id === "delete"){
-            if (screen.textContent.length === 1 || screen.textContent === "ERROR"){
+        btn.addEventListener("click", () => {
+            
+            const btnPushed = btn.textContent;
+    
+            if(btn.id === "c") {
                 screen.textContent = "0";
-            } else {
-                screen.textContent = screen.textContent.slice(0,-1);
-            } return;
-        }
-
-        if(btn.id === "same") {
-            try {
-                screen.textContent = eval(screen.textContent);
-            } catch {
-                screen.textContent = "ERROR";
+                return;
             }
-            return;
-        }
-        
-        if(screen.textContent === "0" || screen.textContent === "ERROR"){
-            screen.textContent = btnPushed;
-        } else {
-            screen.textContent = screen.textContent + btnPushed;
-        }
+    
+            if(btn.id === "delete"){
+                if (screen.textContent.length === 1 || screen.textContent === "ERROR"){
+                    screen.textContent = "0";
+                } else {
+                    screen.textContent = screen.textContent.slice(0,-1);
+                } return;
+            }
+    
+            if(btn.id === "same") {
+                try {
+                    screen.textContent = eval(screen.textContent);
+                } catch {
+                    screen.textContent = "ERROR";
+                }
+                return;
+            }
+            
+            if(screen.textContent === "0" || screen.textContent === "ERROR"){
+                screen.textContent = btnPushed;
+            } else {
+                screen.textContent = screen.textContent + btnPushed;
+            }
+        })
     })
 })
